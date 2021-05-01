b8417d747cf9be7a81edae76f1c66971b97d8b7c
AngelZatch <angelzatch@gmail.com>
2021-05-01 17:27:08 +0200

    chore: simplify files for DI
    Comme l'import/export du contexte de l'orm (l'objet DI) devenait un peu chiant, j'ai regroupé index et application dans un seul fichier

--- app/controllers/auth.controller.ts
+++ app/controllers/auth.controller.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import { DI } from "../index";
 import { User } from "../models/user.model";
 
 const router = Router()
 const SALT_ROUNDS = 10
 
 router.post("/signup", async (request, response) => {
+    const { mail, password, nickname } = request.body;
+    
+    if(await DI.userRepository)
+    
     response.status(503).send('LOL');
 })
 
